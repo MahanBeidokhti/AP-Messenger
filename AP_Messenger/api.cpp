6f3e8e785703a9b82bd5c54a35a13c38b0c5d094
Mahan <mahan.beidokhti1382@gmail.com>
2023-06-25 22:34:48 +0330

    some labels renamed and some of the signals are created

--- AP_Messenger/api.cpp
+++ AP_Messenger/api.cpp
@@ -1,7 +1,20 @@
 #include "api.h"
 
-API::API(QObject *parent)
+API::API(QString Host, QObject *parent)
     : QObject{parent}
 {
+    host = Host;
+    mngr = new QNetworkAccessManager(this);
+    data = new QByteArray();
+}
 
+void API::sign(QString username, QString password, QString Fname, QString Lname)
+{
+    QString command = host+"/signup?username="+username+"&password="+password;
+    if(!Fname.isEmpty())
+        command += "&firstname="+Fname;
+    if(!Lname.isEmpty())
+        command += "&lastname="+Lname;
+    rep = mngr->get(QNetworkRequest(QUrl(command)));
+    connect(rep,&QNetworkReply::finished(),this,&API::Gather());
 }
--- AP_Messenger/api.h
+++ AP_Messenger/api.h
@@ -2,15 +2,27 @@
 #define API_H
 
 #include <QObject>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QByteArray>
+#include <QUrl>
 
 class API : public QObject
 {
     Q_OBJECT
 public:
-    explicit API(QObject *parent = nullptr);
-
+    explicit API(QString host, QObject *parent = nullptr);
+    void sign(QString username, QString password, QString Fname, QString Lname);
+    void log(QString username, QString password);
 signals:
-
+    void NoError(QByteArray* data);
+    void Error(QByteArray* data);
+    void Gather();
+private:
+    QNetworkAccessManager* mngr;
+    QNetworkReply* rep;
+    QByteArray* data;
+    QString host;
 };
 
 #endif // API_H
