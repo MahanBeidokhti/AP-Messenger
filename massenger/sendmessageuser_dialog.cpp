7aa6bf790b86125b2ca0ba391a35b66ef2432c75
Mahan <mahan.beidokhti1382@gmail.com>
2023-06-30 19:18:01 +0330

    send message for user added

--- massenger/sendmessageuser_dialog.cpp
+++ massenger/sendmessageuser_dialog.cpp
@@ -19,9 +19,9 @@ void sendmessageuser_Dialog::on_confirm_pushButton_clicked()
 {
    QString username = ui->username_lineEdit->text();
    ap = new API(("http://api.barafardayebehtar.ml:8080"));
-   //file
+   //file : opening token file and filling string:token with file
    QString tok;
-   ap->chatuser(username,tok);
+   ap->chatload(username,tok,"user");
    connect(ap,&API::UCG_Succ,this,&::sendmessageuser_Dialog::UserChatLoader);
    connect(ap,&API::UCG_Fail,this,&::sendmessageuser_Dialog::UserChatError);
    ui->chat_groupBox->show();
@@ -45,10 +45,34 @@ void sendmessageuser_Dialog::UserChatLoader(QByteArray *data)
     QJsonDocument JAnswer = QJsonDocument::fromJson(*data);
     QJsonObject JV = JAnswer.object();
     QString code =  JV.value("code").toString();
-    //file...
+    //file : writing JV.Value("Block ") to messages file and adding theme to qlistview
 }
 
 void sendmessageuser_Dialog::UserChatError(QNetworkReply *rep)
 {
+    ui->username_lineEdit->setPlaceholderText(rep->errorString());
+}
 
+void sendmessageuser_Dialog::on_send_pushButton_clicked()
+{
+    //file : opening token file and filling string:token with file
+    QString token;
+    ap->sendMessage(ui->send_lineEdit->text(),ui->username_lineEdit->text(),token,"uesr");
+    connect(ap,&API::Send_UCG_Succ,this,&::sendmessageuser_Dialog::UserSendLoader);
+    connect(ap,&API::Send_UCG_Fail,this,&::sendmessageuser_Dialog::UserSendError);
+}
+
+void UserSendLoader(QByteArray *data)
+{
+    QJsonDocument JAnswer = QJsonDocument::fromJson(*data);
+    QJsonObject JV = JAnswer.object();
+    QString code =  JV.value("code").toString();
+    QString message = JV.value("message").toString();
+    qDebug()<<code;
+    qDebug()<<message;
+}
+
+void UserSendError(QNetworkReply *rep)
+{
+    qDebug()<<rep->errorString();
 }
